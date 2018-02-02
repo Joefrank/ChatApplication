f5eb0a273e6e20d86c9f008ac1dee3dc427682d1
Joe Bolla <joe.bolla@wpnchameleon.co.uk>
2018-02-02 07:58:26 +0000

    added project

---
+++ chatapp.web/App_Code/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Owin;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(chatapp.web.App_Code.Startup))]
+namespace chatapp.web.App_Code
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
---
+++ chatapp.web/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNet.SignalR;
+
+namespace chatapp.web.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void Send(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
