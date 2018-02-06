5bb9776583d3a7a6417913ba9e969fcd8d88b646
Joe Bolla <joe.bolla@wpnchameleon.co.uk>
2018-02-06 13:44:29 +0000

    remove old files.

--- UserDetail.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SignalRChat
-{
-    internal class UserDetail
-    {
-        public string ConnectionId { get; set; }
-        public string UserID { get; set; }
-        public string UserName { get; set; }
-    }
-}
